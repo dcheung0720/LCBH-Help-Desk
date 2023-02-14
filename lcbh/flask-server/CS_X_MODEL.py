21718ee3b8e896867d353dfe1337b479bd648101
jdaenzer123 <98292719+jdaenzer123@users.noreply.github.com>
2023-02-14 17:30:25 -0600

    First commit to my own branch

--- lcbh/flask-server/CS_X_MODEL.py
+++ lcbh/flask-server/CS_X_MODEL.py
@@ -10,31 +10,16 @@ class responseGenerator():
     def __init__(self, dataset_file):
         df = pd.read_csv(dataset_file)    #read csv of all past inquiry-response combinations
         df = df.dropna(subset=['Answer']) #remove pairs without a response (all rows in data have an inquiry)
-
-        X = df["Inquiry"]
-        tfidf = TfidfVectorizer()
-        X = tfidf.fit_transform(X)              #vectorize the text inquires for our model
- 
-        #Y = df["Answer"]
-        Y = df[["Answer","Answer Category"]]   #added answer category
-
-        knn = neighbors.KNeighborsClassifier(n_neighbors=1, metric="cosine")     #only one neighbor (find the closest past inquiry)
-        # I think a lot can be done with the distance metric in order to get better results. For instance, we could add a fields... 
-        # for categories, counties and create our own distance function that uses the cosine vector distance but then also makes sure the past response was the same category and county
-
-
-        knn.fit(X,Y)                              #fit the model to the vectorized X data and text Y data
-        self.model = knn
-        self.vectorizer = tfidf                  #save our model and vectorizer for predictions
         self.dataframe = df
-
+        self.fit_responses()
+        
     def get_response(self,inquiry):
         trial = []
         trial.append(inquiry)
         trial_v = self.vectorizer.transform(trial)    #vectorizer inquiry
         #print("trial_v=",trial_v)
         prediction = self.model.predict(trial_v)          #predict response
-        return prediction[0,0], prediction[0,1]
+        return prediction[0,0], prediction[0,1], prediction[0,2]
         #print(preds1)
 
     def add_data(self,inquiry,response,category):
@@ -43,13 +28,16 @@ class responseGenerator():
                     'Answer Category':[category]}
         new_row_df = pd.DataFrame(new_row)
         self.dataframe = pd.concat([self.dataframe, new_row_df],ignore_index = True)
+        self.fit_responses()
+        
 
+    def fit_responses(self):
         X = self.dataframe["Inquiry"]
         tfidf = TfidfVectorizer()
         X = tfidf.fit_transform(X)              #vectorize the text inquires for our model
  
-        #Y = self.dataframe["Answer"]
-        Y = self.dataframe[["Answer","Answer Category"]]
+        #Y = df["Answer"]
+        Y = self.dataframe[["Answer","Answer Category","Inquiry"]]   #added answer category
 
         knn = neighbors.KNeighborsClassifier(n_neighbors=1, metric="cosine")     #only one neighbor (find the closest past inquiry)
         # I think a lot can be done with the distance metric in order to get better results. For instance, we could add a fields... 
@@ -59,8 +47,7 @@ class responseGenerator():
         knn.fit(X,Y)                              #fit the model to the vectorized X data and text Y data
         self.model = knn
         self.vectorizer = tfidf                  #save our model and vectorizer for predictions
-        #print(self.dataframe)
-
+        #self.dataframe = df
         
         
 
