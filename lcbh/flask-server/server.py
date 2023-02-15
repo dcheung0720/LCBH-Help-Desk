3b4186be73970ab5710d06a6ffddd4918f49f748
David Cheung <davidcheung231@gmail.com>
2023-02-15 16:22:50 -0600

    matched_q

--- lcbh/flask-server/server.py
+++ lcbh/flask-server/server.py
@@ -5,15 +5,18 @@ from CS_X_MODEL import responseGenerator
 app = Flask(__name__)
 CORS(app)
 
+rg = responseGenerator(dataset_file= r"Help_Desk_Data_Cleaned_for_Category_Model_Mark_2.csv")
+
 @app.route("/inquiry", methods = ["POST"])
 def home():
     if request.method == "POST":
         text = request.json.get("inquiry")
-        rg = responseGenerator(dataset_file= r"Help_Desk_Data_Cleaned_for_Category_Model_Mark_2.csv")
+
         response = rg.get_response(text)[0]
         cat = rg.get_response(text)[1]
+        matched_ans = rg.get_response(text)[2]
         
-        return jsonify({"inquiry": [response, cat]})
+        return jsonify({"inquiry": [response, cat, matched_ans]})
 
 if __name__ == "__main__":
     app.run(debug= True)
