4d33ad482ddc99671388010da02a669fed42482a
David Cheung <davidcheung231@gmail.com>
2023-03-07 19:07:41 -0600

    more formatting done. Auto populate inquiry, with HTML tags though

--- lcbh/client/src/components/inquiryForm.js
+++ lcbh/client/src/components/inquiryForm.js
@@ -5,7 +5,7 @@ import './inquiryForm.css';
 import SampleResponse from './sampleResponse';
 
 
-function InquiryForm({access_token, conv_id, customerID}){
+function InquiryForm({access_token, conv_id, customerID, threads}){
     const [sampleRes, setSampleRes] = useState([]);
     const [currentText, setCurrentText] = useState('');
     const textChanged = (event) => {
@@ -13,67 +13,70 @@ function InquiryForm({access_token, conv_id, customerID}){
     };
 
     
-    return(<div>
-        <h1>Inquiry</h1>
-        <Formik
-          initialValues={{ inquiry: [] }}
-          onSubmit={(values) => {
-            setTimeout(() => {
-              fetch("http://localhost:5000/inquiry",
-              {
-                method: "POST",
-                headers:{
-                  "Content-Type": 'application/json',
-                  "Access-Control-Allow-Origin": "*"
-                },
-                body: JSON.stringify({
-                  inquiry: values.inquiry
-                })
-              })
-                .then(res => res.json())
-                .then(data =>{ 
-                  // console.log(data.inquiry)
-                  setSampleRes(data.inquiry)
-                })
-                .catch(err => console.log(err))
-            });
-          }}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-            /* and other goodies */
-          }) => (
-            <form>
-              <div className = "input">
-              <textarea
-                name="inquiry"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.inquiry}
-                style = {{"marginRight": "13px"}}
-              />
-              <Button type="submit"  style = {{"backgroundColor": "#005ca4" }} onClick ={handleSubmit} variant="primary">Submit</Button>
-              </div>
-            </form>
-          )}
-        
-        </Formik>
-          <br></br>
-          <SampleResponse 
-            sampleRes={sampleRes}
-            currentText={currentText}
-            setCurrentText={setCurrentText} 
-            access_token={access_token}
-            conv_id={conv_id}
-            customerID={customerID}
-          />
-      </div>)
+    return(<div style = {{"display" : "flex", "justifyContent" : "center", "height" : "500px"}}>
+          <div style = {{"width" : "50%", "margin" : "10px"}}>
+            <h1> Most Recent Inquiry</h1>
+            <Formik
+              initialValues={{ inquiry: [] }}
+              onSubmit={(values) => {
+                setTimeout(() => {
+                  fetch("http://localhost:5000/inquiry",
+                  {
+                    method: "POST",
+                    headers:{
+                      "Content-Type": 'application/json',
+                      "Access-Control-Allow-Origin": "*"
+                    },
+                    body: JSON.stringify({
+                      inquiry: values.inquiry
+                    })
+                  })
+                    .then(res => res.json())
+                    .then(data =>{ 
+                      // console.log(data.inquiry)
+                      setSampleRes(data.inquiry)
+                    })
+                    .catch(err => console.log(err))
+                });
+              }}
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+                /* and other goodies */
+              }) => (
+                <form>
+                  <div className = "input">
+                  <textarea
+                    name="inquiry"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.inquiry}
+                    style = {{"width" : "90%", "marginRight": "13px", "height" : "333px"}}
+                  />
+                  <Button type="submit"  style = {{"backgroundColor": "#005ca4" }} onClick ={handleSubmit} variant="primary">Submit</Button>
+                  </div>
+                </form>
+              )}
+            
+            </Formik>
+          </div>
+          <div style = {{"width" : "50%", "margin" : "10px"}}>
+            <SampleResponse 
+              sampleRes={sampleRes}
+              currentText={currentText}
+              setCurrentText={setCurrentText} 
+              access_token={access_token}
+              conv_id={conv_id}
+              customerID={customerID}
+            />
+          </div>
+        </div>)
 }
 
 
--- lcbh/client/src/components/sampleResponse.js
+++ lcbh/client/src/components/sampleResponse.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import Button from 'react-bootstrap/Button';
 
-function SampleResponse({ sampleRes, currentText, setCurrentText, access_token, conv_id, customerID }) {
+function SampleResponse({ sampleRes, currentText, setCurrentText, access_token, conv_id, customerID}) {
 
     const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -49,32 +49,31 @@ function SampleResponse({ sampleRes, currentText, setCurrentText, access_token, 
 
     return (
     <>
-        <h2>Sample response:</h2>
+        <h1>Sample response:</h1>
         {
             sampleRes.length !== 0 && 
             <>
-                <div className="carousel">
-                    <div style = {{ "width" : "50%"}} className="response">
+                <div className="carousel" >
+                    <div style = {{ "width" : "100%",  "height": "350px"}} className="response">
                         <div style = {{"marginBottom": "10px", "display" : "flex", "justifyContent": "space-between"}}>
                             <Button variant="primary" style = {{"backgroundColor": "#005ca4" }} onClick={handlePrevClick}>Prev </Button>
                             <Button variant="primary"  style = {{"backgroundColor": "#005ca4" }} onClick={handleNextClick}>Next </Button>
                         </div>
-                        <div style={{"borderStyle" : "solid", "marginBottom": "10px", "padding": "10px"}}>
+                        <div style={{"borderStyle" : "solid", "marginBottom": "10px", "padding": "10px", "height": "30%", "overflowY" : "scroll"}}>
                             <p> <b>Category:</b> {sampleRes[currentIndex][1]}</p>
                             <p> <b>Original Inquiry:</b> {sampleRes[currentIndex][2]} </p>
                         </div>
-                        <div style = {{"display" : "flex", "alignItems" : "center"}}>
-                            <div style={{"width" : "100%"}}>
+                        <div style = {{"display" : "flex", "alignItems" : "center", "height": "50%"}}>
+                            <div style={{"width" : "100%", "height" : "100%"}}>
                                 <textarea 
-                                    style={{ "height": "200px", "width": "100%"}}
+                                    style={{"width": "100%", "height" : "100%", "padding": "none"}}
                                     value={currentText}
                                     onChange={(e) => {
-
                                         setCurrentText(e.target.value);
                                     }} > 
                                 </textarea>
                             </div>  
-                            <Button variant="primary"  style = {{"backgroundColor": "#005ca4", "height" : "50%", "marginLeft" : "20px" }} onClick={createReplyToThread}>Send to HelpScout</Button>
+                            <Button variant="primary"  style = {{"backgroundColor": "#005ca4", "height" : "40%", "marginLeft" : "20px" }} onClick={createReplyToThread}>Send to HelpScout</Button>
                         </div>                     
                     </div>
                 
